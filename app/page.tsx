64b2fb489a2783dfb0b762c0ebf7257ae0225eca
fcvirtual <austin@fcvt.dev>
2023-10-23 13:00:40 -0400

    Feat: Tailwind Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
-      </div>
-    </main>
-  )
+    <div>
+      <h1 className="h1-bold">Next JS 13</h1>
+      <div><div></div></div>
+    </div>
+  );
 }
