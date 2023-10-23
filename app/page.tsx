0d233589738d1564b1811158935b1524f125f4f3
fcvirtual <austin@fcvt.dev>
2023-10-23 13:22:17 -0400

    feat: Folder Structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <div>
       <h1 className="h1-bold">Next JS 13</h1>
+      <p> Folder Structure </p>
+      <p>
+        <p></p>
+      </p>
     </div>
   );
 }
