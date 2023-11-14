b0b3796c47d03582bbc1651147e7dbd706d8a041
fcvirtual <austin@fcvt.dev>
2023-11-14 16:39:54 -0500

    feat: Start QuestionCard

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
 import HomeFilters from "@/components/shared/home/HomeFilters";
@@ -7,32 +8,40 @@ import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
 const questions = [
-  // {
-  //   _id: 1,
-  //   title: "Cascading Delets in SQLAlchemy",
-  //   tags: [
-  //     { _id: 1, name: "python" },
-  //     { _id: 2, name: "sql" },
-  //   ],
-  //   author: "John Doe",
-  //   upvotes: 10,
-  //   views: 100,
-  //   answers: 2,
-  //   createdAt: "2023-09",
-  // },
-  // {
-  //   _id: 2,
-  //   title: "Something Else",
-  //   tags: [
-  //     { _id: 1, name: "python" },
-  //     { _id: 2, name: "sql" },
-  //   ],
-  //   author: "John Doe",
-  //   upvotes: 10,
-  //   views: 100,
-  //   answers: 2,
-  //   createdAt: "2023-09",
-  // },
+  {
+    _id: "1",
+    title: "Cascading Delets in SQLAlchemy",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sql" },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+      picture: "https://example.com/johndoe.jpg",
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [{}, {}],
+    createdAt: new Date("2023-09"),
+  },
+  {
+    _id: "2",
+    title: "Something Else",
+    tags: [
+      { _id: "1", name: "python" },
+      { _id: "2", name: "sql" },
+    ],
+    author: {
+      _id: "2",
+      name: "John Doe",
+      picture: "https://example.com/johndoe2.jpg",
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [{}, {}],
+    createdAt: new Date("2023-09"),
+  },
 ];
 
 export default function Home() {
@@ -63,7 +72,19 @@ export default function Home() {
       <HomeFilters />
       <div className="mt-10 flex w-full flex-col gap-6">
         {questions.length > 0 ? (
-          questions.map((question) => "QuestionCard")
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
         ) : (
           <NoResult
             title="There's no questions to show"
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -6,34 +6,34 @@ import RenderTag from "./RenderTag";
 const RightSidebar = () => {
   const hotQuestions = [
     {
-      _id: 1,
+      _id: "1",
       title:
         "Would it be appropriate to point out an error in another paper during a referee report?",
     },
     {
-      _id: 2,
+      _id: "2",
       title: "How can an airconditioning machine exist?",
     },
     {
-      _id: 3,
+      _id: "3",
       title: "Interrogated every time crossing UK Border as citizen",
     },
     {
-      _id: 4,
+      _id: "4",
       title: "Low digit addition generator",
     },
     {
-      _id: 5,
+      _id: "5",
       title: "What is an example of 3 numbers that do not make up a vector?",
     },
   ];
 
   const popularTags = [
-    { _id: 1, name: "Javascript", totalQuestions: 5 },
-    { _id: 2, name: "React", totalQuestions: 5 },
-    { _id: 3, name: "Next", totalQuestions: 5 },
-    { _id: 4, name: "Vue", totalQuestions: 2 },
-    { _id: 5, name: "Redux", totalQuestions: 10 },
+    { _id: "1", name: "Javascript", totalQuestions: 5 },
+    { _id: "2", name: "React", totalQuestions: 5 },
+    { _id: "3", name: "Next", totalQuestions: 5 },
+    { _id: "4", name: "Vue", totalQuestions: 2 },
+    { _id: "5", name: "Redux", totalQuestions: 10 },
   ];
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
