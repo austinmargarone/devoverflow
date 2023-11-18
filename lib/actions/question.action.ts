13d022c5ea269236864417658ecd5857ed534dc4
fcvirtual <austin@fcvt.dev>
2023-11-18 11:33:33 -0500

    feat: createQuestion Server Action

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,10 +1,32 @@
 "use server";
 
+import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
 
 export async function createQuestion(params: any) {
   // eslint-disable-next-line no-undef
   try {
     connectToDatabase();
+    const { title, content, tags, author } = params;
+
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+
+    const tagDocuments = [];
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag }, $push: { questions: question._id } },
+        { upsert: true, new: true }
+      );
+      tagDocuments.push(existingTag._id);
+    }
+    await question.findOneAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
   } catch (error) {}
 }
