8d1d8f41395b42946fb3bab7164104f172ded610
fcvirtual <austin@fcvt.dev>
2023-12-06 17:39:06 -0500

    feat: Display Answer

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -6,11 +6,9 @@ import Theme from "./Theme";
 import MobileNav from "./MobileNav";
 import GlobalSearch from "../search/GlobalSearch";
 
-type Props = {};
-
-const Navbar = (props: Props) => {
+const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -18,13 +16,17 @@ const Navbar = (props: Props) => {
           height={23}
           alt="DevFlow"
         />
+
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-          Dev <span className="text-primary-500">OverFlow</span>
+          Dev <span className="text-primary-500">Overflow</span>
         </p>
       </Link>
+
       <GlobalSearch />
+
       <div className="flex-between gap-5">
         <Theme />
+
         <SignedIn>
           <UserButton
             afterSignOutUrl="/"
@@ -38,6 +40,7 @@ const Navbar = (props: Props) => {
             }}
           />
         </SignedIn>
+
         <MobileNav />
       </div>
     </nav>
