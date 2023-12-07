a08003806f04325504fc5e8ad9a476f26bfefe50
fcvirtual <austin@fcvt.dev>
2023-12-07 15:31:39 -0500

    feat: Answers

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -21,7 +21,6 @@ export async function createAnswer(params: CreateAnswerParams) {
 
     const newAnswer = await Answer.create({ content, author, question });
 
-    // Add the answer to the question's answers array
     const questionObject = await Question.findByIdAndUpdate(question, {
       $push: { answers: newAnswer._id },
     });
