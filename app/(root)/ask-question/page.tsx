10aaac71d81e941338f5ab1fec9548ac151457f9
fcvirtual <austin@fcvt.dev>
2023-12-14 16:46:26 -0500

    feat: edit question

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -12,13 +12,13 @@ const Page = async () => {
   const mongoUser = await getUserById({ userId });
 
   return (
-    <>
+    <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
 
       <div className="mt-9">
         <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
       </div>
-    </>
+    </div>
   );
 };
 
