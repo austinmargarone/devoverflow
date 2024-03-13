036a2e8201ac3495aeb48b280007a80ec33ae8b7
fcvirtual <austin@fcvt.dev>
2024-03-13 13:57:31 -0400

    fix: Bugs

--- components/shared/EditDeleteAction.tsx
+++ components/shared/EditDeleteAction.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { deleteAnswer } from "@/lib/actions/answer.action";
 import { deleteQuestion } from "@/lib/actions/question.action";
 import Image from "next/image";
@@ -14,27 +15,29 @@ const EditDeleteAction = ({ type, itemId }: Props) => {
   const router = useRouter();
 
   const handleEdit = () => {
-    router.push(`/question/edit/${JSON.parse(itemId)}`);
+    router.push(`/question/edit/${JSON.parse(itemId)}`)
   };
 
   const handleDelete = async () => {
-    if (type === "Question") {
-      await deleteQuestion({
-        questionId: JSON.parse(itemId),
-        path: pathname,
-      });
-    } else if (type === "Answer") {
-      await deleteAnswer({
-        answerId: JSON.parse(itemId),
-        path: pathname,
-      });
+    if(type === 'Question') {
+      // Delete question
+      await deleteQuestion({ 
+        questionId: JSON.parse(itemId), 
+        path: pathname 
+      })
+    } else if(type === 'Answer') {
+      // Delete answer
+      await deleteAnswer({ 
+        answerId: JSON.parse(itemId), 
+        path: pathname 
+      })
     }
   };
 
   return (
     <div className="flex items-center justify-end gap-3 max-sm:w-full">
-      {type === "Question" && (
-        <Image
+      {type === 'Question' && (
+        <Image 
           src="/assets/icons/edit.svg"
           alt="Edit"
           width={14}
@@ -44,16 +47,16 @@ const EditDeleteAction = ({ type, itemId }: Props) => {
         />
       )}
 
-      <Image
-        src="/assets/icons/trash.svg"
-        alt="Delete"
-        width={14}
-        height={14}
-        className="cursor-pointer object-contain"
-        onClick={handleDelete}
-      />
+        <Image 
+          src="/assets/icons/trash.svg"
+          alt="Delete"
+          width={14}
+          height={14}
+          className="cursor-pointer object-contain"
+          onClick={handleDelete}
+        />
     </div>
-  );
-};
+  )
+}
 
-export default EditDeleteAction;
+export default EditDeleteAction
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import { getHotQuestions } from "@/lib/actions/question.action";
-import { getTopPopularTags } from "@/lib/actions/tag.actions";
-import RenderTag from "./RenderTag";
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+import RenderTag from './RenderTag';
+import { getHotQuestions } from '@/lib/actions/question.action';
+import { getTopPopularTags } from '@/lib/actions/tag.actions';
 
 const RightSidebar = async () => {
   const hotQuestions = await getHotQuestions();
-
   const popularTags = await getTopPopularTags();
 
   return (
@@ -19,14 +18,12 @@ const RightSidebar = async () => {
             <Link
               href={`/question/${question._id}`}
               key={question._id}
-              className="flex cursor-pointer items-center justify-between gap-7"
+              className='flex cursor-pointer items-center justify-between gap-7'
             >
-              <p className="body-medium text-dark500_light700">
-                {question.title}
-              </p>
-              <Image
-                src={"./assets/icons/arrow-right.svg"}
-                alt={"Arrow-Right"}
+              <p className="body-medium text-dark500_light700">{question.title}</p>
+              <Image 
+                src="/assets/icons/chevron-right.svg"
+                alt="chevron right"
                 width={20}
                 height={20}
                 className="invert-colors"
@@ -36,21 +33,21 @@ const RightSidebar = async () => {
         </div>
       </div>
       <div className="mt-16">
-        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+      <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
         <div className="mt-7 flex flex-col gap-4">
-          {popularTags.map((tag) => (
-            <RenderTag
-              key={tag._id}
-              _id={tag._id}
-              name={tag.name}
-              totalQuestions={tag.numberOfQuestions}
-              showCount
-            />
-          ))}
+            {popularTags.map((tag) => (
+              <RenderTag 
+                key={tag._id}
+                _id={tag._id}
+                name={tag.name}
+                totalQuestions={tag.numberOfQuestions}
+                showCount
+              />
+            ))}
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default RightSidebar;
+export default RightSidebar
